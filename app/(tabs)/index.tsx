d544880a7b54b4b823c882fa5ca05b9917ec8ede
Zachary <zewigfield@gmail.com>
2025-05-01 02:24:55 -0400

    edited landing screen text and removed button since logging screen will be access by selecting a calendar date

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,12 +8,8 @@ import CalendarView from "components/CalendarView";
 export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-2xl font-bold mb-4">Migraine Tracker</Text>
-      <Link href="/log-day" asChild>
-        <Pressable className="bg-blue-500 px-4 py-2 rounded-xl">
-          <Text className="text-white text-lg">Log Today</Text>
-        </Pressable>
-      </Link>
+      <Text className="text-2xl font-bold mb-4">Health Tracker</Text>
+      <Text>Select a date to log health factors</Text>
       <CalendarView />
     </View>
   );
