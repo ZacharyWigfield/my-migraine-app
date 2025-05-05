491cba973d33eecf177650e6ae6d33a9f5e2f3e7
Zachary <zewigfield@gmail.com>
2025-05-04 23:58:40 -0400

    added logout button and functionality

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,15 +2,28 @@ import { View, Text, Pressable } from "react-native";
 import { Link } from "expo-router";
 import "global.css"
 import CalendarView from "components/CalendarView";
+import { signOut } from "firebase/auth";
+import { auth } from "../../lib/firebase";
 
 // landing page once a user is authenticated. Default landing page if a user is already signed in
 
 export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
+      <Pressable className="bg-red-500 px-4 py-2 rounded" onPress={handleLogout}>
+        <Text className="text-white">Logout</Text>
+      </Pressable>
       <Text className="text-2xl font-bold mb-4">Health Tracker</Text>
       <Text>Select a date to log health factors</Text>
       <CalendarView />
     </View>
   );
+}
+
+async function handleLogout() {
+  try {
+    await signOut(auth);
+  } catch (error) {
+    console.error("Error signing out:", error);
+  }
 }
