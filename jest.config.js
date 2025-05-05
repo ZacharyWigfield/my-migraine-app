2a239c107e23060e289036c34200354f5b778b4e
Zachary <zewigfield@gmail.com>
2025-05-05 06:15:58 -0400

    changes to test config, it still isn't working. Will continue to work on it. Added configuration for app.json to support native firebase modules that are currently causing error.

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,13 @@ module.exports = {
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   testEnvironment: 'jsdom',
   transformIgnorePatterns: [
-    'node_modules/(?!(expo(nent)?|@expo(nent)?|react-native|@react-native|@react-navigation|nativewind|expo-modules-core)/)',
+    'node_modules/(?!(expo(nent)?|@expo(nent)?|react-native|@react-native|@react-navigation|nativewind|expo-modules-core|@react-native-firebase)/)',
   ],
+  collectCoverage: true,  // Enables code coverage collection
+  coverageDirectory: 'coverage',  // Where the coverage report will be saved
+  testTimeout: 15000,  // 15-second timeout for slow tests (optional)
+  moduleNameMapper: {
+    '\\.css$': 'jest-transform-stub',  // Mock CSS imports
+    '^components/(.*)$': '<rootDir>/components/$1',  // Resolve components directory
+  },
 };
