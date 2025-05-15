c11cf21966f25ccf61750342336948d787d4adce
Zachary <zewigfield@gmail.com>
2025-05-15 02:08:40 -0400

    added logic to write date entry form data object to firebase

--- app/(tabs)/entry/[date].tsx
+++ app/(tabs)/entry/[date].tsx
@@ -1,6 +1,10 @@
 import DateEntryForm from "components/DateEntryForm";
 import { useLocalSearchParams } from "expo-router";
 import { DateEntryFormData } from "types/dateEntryFormData";
+import { getAuth } from '@react-native-firebase/auth';
+import { getFirestore, doc, setDoc } from '@react-native-firebase/firestore';
+import { Alert } from "react-native";
+
 
 export default function DateEntry() {
   const { date } = useLocalSearchParams();
@@ -20,9 +24,25 @@ export default function DateEntry() {
     notes: "",
   }
 
-  const onSubmit = (data: DateEntryFormData) => {
-    console.log("Form submitted:", data);
-    // Send data to Firebase, etc.
+  const onSubmit = async (data: DateEntryFormData) => {
+    try {
+      await saveEntry(data);
+      //replace with toast
+      console.log('Entry saved!');
+    } catch (e) {
+      Alert.alert("Submit Failed", `Failed to save entry: ${e}`)
+    }
+  };
+
+  const saveEntry = async (data: DateEntryFormData) => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+    if (!user) throw new Error("User not authenticated");
+
+    const docId = `${user.uid}_${data.date.toISOString().split('T')[0]}`;
+    const db = getFirestore();
+    const ref = doc(db, 'entries', docId);
+    await setDoc(ref, data);
   };
 
   return (
