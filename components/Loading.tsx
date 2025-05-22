537f5f569bdac71f82c9ea7d7a84c85c017802b0
Zachary <zewigfield@gmail.com>
2025-05-22 15:09:24 -0400

    added dark mode styling to loading component

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -5,7 +5,7 @@ const colorScheme = useColorScheme();
 
 export default function LoadingSpinner() {
   return (
-    <View className="flex-1 justify-center items-center bg-white dark:bg-migraineSafe-bg">
+    <View className="flex-1 justify-center items-center bg-lightBg dark:bg-migraineSafeBg">
       <ActivityIndicator size="large" color={colorScheme === "dark" ? "#E0E0E0" : "#111827"} />
     </View>
   );
